639a4fb5e147e76a9b83e0c10f008032f7e47de0
ARitik <ambadiritik@gmail.com>
2022-03-26 21:57:48 +0530

    Updated emacs.d.gnu | changed init.el to allow for transparent title bars : Darwin

--- .emacs.d.gnu/init.el
+++ .emacs.d.gnu/init.el
@@ -2,4 +2,18 @@
  (expand-file-name
   "config.org"
   user-emacs-directory))
-
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+(add-to-list 'default-frame-alist '(ns-appearance . dark))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(peep-dired all-the-icons-ibuffer all-the-icons-dired dashboard projectile general which-key doom-modeline key-chord evil-commentary evil-collection evil use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
