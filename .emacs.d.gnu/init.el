40e35c9427f6bda0b7b0ada347bfb75587c40fb3
ARitik <ambadiritik@gmail.com>
2022-04-04 02:22:15 +0530

    Completion framework

--- .emacs.d.gnu/init.el
+++ .emacs.d.gnu/init.el
@@ -1,3 +1,10 @@
+;;; init.el --- Initialization file for Emacs
+
+;;; Commentary:
+;; Configuration file :: config.org
+
+;;; Code:
+
 (org-babel-load-file
  (expand-file-name
   "config.org"
@@ -16,12 +23,16 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("a9a67b318b7417adbedaab02f05fa679973e9718d9d26075c6235b1f0db703c8" default))
+   '("9b54ba84f245a59af31f90bc78ed1240fca2f5a93f667ed54bbf6c6d71f664ac" "d268b67e0935b9ebc427cad88ded41e875abfcc27abd409726a92e55459e0d01" "b186688fbec5e00ee8683b9f2588523abdf2db40562839b2c5458fcfb322c8a4" default))
  '(package-selected-packages
-   '(toc-org marginalia undo-tree vertico rainbow-delimiters rainbow-mode projectile which-key all-the-icons-ibuffer all-the-icons-dired helpful dashboard doom-modeline peep-dired general key-chord evil-commentary evil-collection evil doom-themes gcmh use-package)))
+   '(toc-org marginalia vertico rainbow-delimiters rainbow-mode projectile which-key all-the-icons-ibuffer all-the-icons-dired dashboard doom-modeline general key-chord evil-commentary evil-collection evil doom-themes gcmh use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(provide 'init)
+
+;;; init.el ends here
