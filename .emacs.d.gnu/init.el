fa9f95c940072f96dcb0bbd82f497a83d7903b27
ARitik <ambadiritik@gmail.com>
2022-04-14 18:16:14 +0530

    LSP Support

--- .emacs.d.gnu/init.el
+++ .emacs.d.gnu/init.el
@@ -25,13 +25,18 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(visual-fill-column toc-org marginalia vertico rainbow-delimiters rainbow-mode projectile which-key all-the-icons-ibuffer all-the-icons-dired dashboard doom-modeline general key-chord evil-commentary evil-collection evil doom-themes gcmh use-package)))
+   '(lsp-pyright lsp-mode evil-mc visual-fill-column marginalia vertico rainbow-delimiters rainbow-mode projectile which-key all-the-icons-ibuffer all-the-icons-dired dashboard doom-modeline general key-chord evil-commentary evil-collection evil doom-themes gcmh use-package)))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
-(provide 'init)
+ '(org-document-title ((t (:inherit outline-1 :height 1.4))))
+ '(org-level-1 ((t (:inherit outline-1 :height 1.15))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.1))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.0)))))
+
+ (provide 'init)
 
 ;;; init.el ends here
